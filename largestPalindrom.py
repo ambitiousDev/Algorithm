01c867ca1cd4cc69f13afbd855ffe63c6f81220e
Tien Anh Duong <duongtienanh1911@gmail.com>
2020-09-21 09:06:39 +0700

    largest palindrome

---
+++ largestPalindrom.py
@@ -0,0 +1,28 @@
+import time
+
+
+def largestPalindrom(digits):
+    min, max, temp, pal = '1', '', 0, 0
+    for _ in range(1, digits):
+        min += '0'
+    for _ in range(0, digits):
+        max += '9'
+
+    min = int(min)-1
+    max = int(max)
+    start = time.time()
+    for i in range(max, min, -1):
+        for j in range(i, min, -1):
+            temp = i*j
+            if temp < pal:
+                break
+            if str(temp) == "".join(reversed(str(temp))):
+                pal = temp
+    end = time.time()
+
+    print(pal, end=' ')
+    print(end-start)
+
+
+if __name__ == '__main__':
+    largestPalindrom(3)
